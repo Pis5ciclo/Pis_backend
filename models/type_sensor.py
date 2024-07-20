7a6482350b48e287bcadd9fe2103941f8a8b16d5
Santiago <santiabrigo497@gmail.com>
2024-07-20 10:35:28 -0500

    endopoint types

--- models/type_sensor.py
+++ models/type_sensor.py
@@ -2,9 +2,13 @@ from enum import Enum
 class TypeSensor(Enum):
     AGUA = 'Agua'
     AIRE = 'Aire'
+    
     @property
     def serialize(self):
         return {
-            'name': self.name
+            'name': self.value  # Usa .value para obtener el valor del Enum
         } 
-    
+
+    @classmethod
+    def all_types(cls):
+        return [sensor.serialize for sensor in cls]
